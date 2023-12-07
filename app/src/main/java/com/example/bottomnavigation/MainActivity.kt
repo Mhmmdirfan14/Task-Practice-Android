56546175075b8bc0f58556c3595c4f3ae14fd9e8
Muhammad Irfan Hakim <muhammadirfanhakim45@gmail.com>
2023-12-08 00:05:58 +0700

    bottom menu & animations

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+
+                R.id.setting -> {
+                    replaceFragment(SettingsFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+}
